3273ce2d74095abb4e1f9ea0b448c4d90dfea732
Mohsin Zaidi <mzaidi@bandwidth.com>
2020-07-09 00:01:24 -0400

    Cleanup

--- src/DTMFSender.ts
+++ src/DTMFSender.ts
@@ -15,19 +15,15 @@ class DTMFSender {
     ["1", [1209, 697]],
     ["2", [1336, 697]],
     ["3", [1477, 697]],
-    ["a", [1633, 697]],
     ["4", [1209, 770]],
     ["5", [1336, 770]],
     ["6", [1477, 770]],
-    ["b", [1633, 770]],
     ["7", [1209, 852]],
     ["8", [1336, 852]],
     ["9", [1477, 852]],
-    ["c", [1633, 852]],
     ["*", [1209, 941]],
     ["0", [1336, 941]],
-    ["#", [1477, 941]],
-    ["d", [1633, 941]],
+    ["#", [1477, 941]]
   ]);
 
   constructor(sender: RTCRtpSender) {
@@ -49,27 +45,27 @@ class DTMFSender {
 
     this._osc1 = audioCtx.createOscillator();
     this._osc1.connect(this._outputNode);
-    this._osc1.type = 'sine';
+    this._osc1.type = "sine";
     this._osc1.frequency.value = 0;
     this._osc1.start(0);
 
     this._osc2 = audioCtx.createOscillator();
     this._osc2.connect(this._outputNode);
-    this._osc2.type = 'sine';
+    this._osc2.type = "sine";
     this._osc2.frequency.value = 0;
     this._osc2.start(0);
 
-	this._gain = audioCtx.createGain();
-	this._gain.gain.value = 0.25;
+    this._gain = audioCtx.createGain();
+    this._gain.gain.value = 0.25;
 
-	this._filter = audioCtx.createBiquadFilter();
-	this._filter.type = "lowpass";
+    this._filter = audioCtx.createBiquadFilter();
+    this._filter.type = "lowpass";
 
-	this._osc1.connect(this._gain);
-	this._osc2.connect(this._gain);
+    this._osc1.connect(this._gain);
+    this._osc2.connect(this._gain);
 
-	this._gain.connect(this._filter);
-	this._filter.connect(audioCtx.destination);
+    this._gain.connect(this._filter);
+    this._filter.connect(audioCtx.destination);
 
     if (rtpSender) {
       rtpSender.replaceTrack(this._outputStream.getAudioTracks()[0]);
@@ -80,7 +76,7 @@ class DTMFSender {
 
   insertDTMF(tone: string, duration: number, toneGap: number) {
     if ((tone.length != 1) || (/[^0-9a-d#\*,]/i.test(tone))) {
-        throw new Error("Invalid digit");
+      throw new Error("Invalid tone");
     }
 
     this._toneDuration = Math.min(6000, Math.max(40, duration || 100));
@@ -88,36 +84,29 @@ class DTMFSender {
     this._tone = tone;
 
     if (!this._playing) {
-      setTimeout(this._playNextTone.bind(this), 0);
+      setTimeout(this._playTone.bind(this), 0);
       this._playing = true;
     }
   }
 
-  _playNextTone() {
-    if (this._tone.length == 0) {
-      this._playing = false;
-      this._osc1.frequency.value = 0;
-      this._osc2.frequency.value = 0;
-      return;
-    }
-
-    var digit: string = this._tone.substr(0, 1);
-    this._tone = this._tone.substr(1);
-
+  _playTone() {
+    var digit: string = this._tone[0];
     var f = this._dtmfFreq.get(digit.toLowerCase());
+
+    // Stop the tone immediately if frequencies are not found
+    var toneDuration: number = 0;
     if (f) {
       this._osc1.frequency.value = f[0];
       this._osc2.frequency.value = f[1];
-      setTimeout(this._stopTone.bind(this), this._toneDuration);
-    } else {
-      setTimeout(this._playNextTone.bind(this), 0);
+      toneDuration = this._toneDuration;
     }
+    setTimeout(this._stopTone.bind(this), toneDuration);
   }
 
   _stopTone() {
+    this._playing = false;
     this._osc1.frequency.value = 0;
     this._osc2.frequency.value = 0;
-    setTimeout(this._playNextTone.bind(this), this._toneGap);
   }
 }
 
