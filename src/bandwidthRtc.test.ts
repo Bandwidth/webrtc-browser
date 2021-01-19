0422aefcee38b804a765b6ac2519bad8a0c4197a
Chris Arrington <carrington@bandwidth.com>
2021-01-19 11:11:00 -0500

    EDGE-618 Run prettier on bandwidthRtc.test.ts

--- src/bandwidthRtc.test.ts
+++ src/bandwidthRtc.test.ts
@@ -33,40 +33,40 @@ test("test getMediaDevices", async () => {
   const bandwidthRtc = new BandwidthRtc();
 
   navigator.mediaDevices = jest.mock();
-  navigator.mediaDevices.enumerateDevices = jest.fn(() => Promise.resolve(
-    [
+  navigator.mediaDevices.enumerateDevices = jest.fn(() =>
+    Promise.resolve([
       {
-        "deviceId": "default",
-        "kind": "audioinput",
-        "label": "Default",
-        "groupId": "cbbd856b3fa6b1a8e87def7ccf8b715473302cb565823f6a7f311cb46325279c"
+        deviceId: "default",
+        kind: "audioinput",
+        label: "Default",
+        groupId: "cbbd856b3fa6b1a8e87def7ccf8b715473302cb565823f6a7f311cb46325279c",
       },
       {
-        "deviceId": "b5aaf23936d9c9cdf6aed368cc8a35f51b081611e2aad97d0379779f43bbe4e6",
-        "kind": "audioinput",
-        "label": "OrbiCam Analog Stereo",
-        "groupId": "65cb96a57369e87ce2d88921df3a95aa87464ad3afd32a6710860f69be919d90"
+        deviceId: "b5aaf23936d9c9cdf6aed368cc8a35f51b081611e2aad97d0379779f43bbe4e6",
+        kind: "audioinput",
+        label: "OrbiCam Analog Stereo",
+        groupId: "65cb96a57369e87ce2d88921df3a95aa87464ad3afd32a6710860f69be919d90",
       },
       {
-        "deviceId": "b1f3a3c207db50447a7d9526fdbeeca5118d23023eb9cd60ab487dc1e552e944",
-        "kind": "videoinput",
-        "label": "HD Pro Webcam C920 (046d:0892)",
-        "groupId": "a06082a517d049ddb91b14623b5acc04c4445b9aadafead8ead53530dd4d1555"
+        deviceId: "b1f3a3c207db50447a7d9526fdbeeca5118d23023eb9cd60ab487dc1e552e944",
+        kind: "videoinput",
+        label: "HD Pro Webcam C920 (046d:0892)",
+        groupId: "a06082a517d049ddb91b14623b5acc04c4445b9aadafead8ead53530dd4d1555",
       },
       {
-        "deviceId": "default",
-        "kind": "audiooutput",
-        "label": "Default",
-        "groupId": "default"
+        deviceId: "default",
+        kind: "audiooutput",
+        label: "Default",
+        groupId: "default",
       },
       {
-        "deviceId": "dd7c210a57368a97a5133b617ad3a8eba85507c05fc9ca13724364fe4122a0a7",
-        "kind": "audiooutput",
-        "label": "Built-in Audio Analog Stereo",
-        "groupId": "f196720f272a589069985685323a804ba77a981f0259c0a423dd666876472103"
-      }
-    ]
-  ));
+        deviceId: "dd7c210a57368a97a5133b617ad3a8eba85507c05fc9ca13724364fe4122a0a7",
+        kind: "audiooutput",
+        label: "Built-in Audio Analog Stereo",
+        groupId: "f196720f272a589069985685323a804ba77a981f0259c0a423dd666876472103",
+      },
+    ])
+  );
 
   let devices = await bandwidthRtc.getMediaDevices();
   expect(devices.length).toBe(5);
