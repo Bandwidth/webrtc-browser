a1e9fbd39e9f6d6a96f2981ab0e314a80657f406
Chris Johnson <cejohnson@bandwidth.com>
2021-04-02 15:39:28 -0400

    Fix index.ts typo

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { default } from "./bandwidthRtc";
-export { BandwidthRtc as BandwidthRtcV2 } from "./v3/bandwidthRtc";
+export { BandwidthRtc as BandwidthRtcV2 } from "./v2/bandwidthRtc";
 export { BandwidthRtc as BandwidthRtcV3 } from "./v3/bandwidthRtc";
 export * from "./types";
